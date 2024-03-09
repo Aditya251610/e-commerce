57f6e3a6e64bb469827058e778b57bf7ccefcc6a
Aditya251610 <aditya.sharma16062016@gmail.com>
2024-03-09 23:10:54 +0530

    integrated the login and signup functionality with the frontend

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -31,7 +31,10 @@ const Navbar = () => {
             <li onClick={() =>{setMenu("Kids")}}><Link style={{textDecoration: "none"}} to='/kids'>Kids</Link>{menu==="Kids"?<hr />:null}</li>
         </ul>
         <div className='nav-login-cart'>
-            <Link to='/login'><button>Login</button></Link>
+            {localStorage.getItem('auth-token')
+            ?<button onClick={()=>{localStorage.removeItem('auth-token');window.location.replace('/')}}>Logout</button>
+            :<Link to='/login'><button>Login</button></Link>}
+            
             <Link to='/cart'><img src={cart_icon} alt='car-icon' /></Link>
             <div className='nav-cart-count'>{getTotalItem()}</div>
         </div>
